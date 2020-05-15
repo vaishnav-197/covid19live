a0159f64d2809b9c4d549050788065f95bece369
vaishnav-197 <46111025+vaishnav-197@users.noreply.github.com>
2020-05-16 01:39:58 +0530

    added material UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
-
-import styles from './App.module.css'
+import {fetchData} from './api';
+import styles from './App.module.css';
 import {Cards , Chart ,CountryPicker} from './components';
 
 class App extends React.Component {
 
+    state= {
+        data : {}
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({data : fetchedData});
+
+
+       
+
+    }
     render () {
+        const { data } = this.state;
+
         return (
-            <div>
-                <Cards />
+            <div className={styles.container}>
+                <Cards data = {data} />
                 <CountryPicker />
                 <Chart />
             </div>
